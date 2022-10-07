fc30268e78c65f3a90357aa2a0e33f9990e242fd
GsnMithra <mithragsn9@gmail.com>
2022-10-07 13:06:58 +0530

    application

--- testmongo/app.js
+++ testmongo/app.js
@@ -1,7 +1,18 @@
-const mongoose = require("mongoose");
+async function entry() {
+    const data = document.getElementById('given').value;
+    const { MongoClient } = require('mongodb');
+    const url = 'mongodb://localhost:27017';
+    const client = new MongoClient(url);
+    const database = 'primary';
+    
+    await client.connect();
+    console.log('connected!');
+    const db = client.db(database);
+    const collection = db.collection('users');
+    collection.insertOne({ name : data });
+    db.close();
+}
 
-mongoose.connect("mongodb://localhost:27017/test/", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+entry()
+    .then(console.log)
+    .catch(console.error)
